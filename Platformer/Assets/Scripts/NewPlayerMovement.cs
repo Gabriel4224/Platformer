6e5eb527153d4ee14298551ecae6f7a8e93634f9
Gabriel4224 <31612343+Gabriel4224@users.noreply.github.com>
2018-11-19 22:00:51 +1100

    Added Sprint and doublejump to player

--- Platformer/Assets/Scripts/NewPlayerMovement.cs
+++ Platformer/Assets/Scripts/NewPlayerMovement.cs
@@ -10,6 +10,10 @@ public class NewPlayerMovement : MonoBehaviour {
     private float YAxis;
 
     public float Speed;
+
+    float DefaultSpeed;
+    bool IsSprinting;
+    int SprintToggle;
     public float Jump;
 
     bool CanJump;
@@ -20,42 +24,79 @@ public class NewPlayerMovement : MonoBehaviour {
     Vector3 moveDirection;
     int Djump;
     // Use this for initialization
-    void Start () {
+    void Start() {
         //        rb = GetComponent<Rigidbody>();
         CharControl = GetComponent<CharacterController>();
 
-
+        DefaultSpeed = Speed ;
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
 
+         Movement();
+     
+    }
+
+    void Movement()
+    {
         XAxis = XCI.GetAxis(XboxAxis.LeftStickX);
         YAxis = XCI.GetAxis(XboxAxis.LeftStickY);
 
         // moveDirection = new Vector3(XAxis * Speed, moveDirection.y, YAxis * Speed);
 
         float YStore = moveDirection.y;
-        moveDirection = (Camera.main.transform.forward * YAxis  ) + (Camera.main.transform.right * XAxis  );
+        moveDirection = (Camera.main.transform.forward * YAxis) + (Camera.main.transform.right * XAxis);
         moveDirection = moveDirection.normalized * Speed;
         moveDirection.y = YStore;
+        // Jump 
         if (CharControl.isGrounded)
         {
             moveDirection.y = 0;
             if (XCI.GetButtonDown(XboxButton.A))
             {
- 
+
                 moveDirection.y = Jump;
- 
+                CanJump = true;
+            }
+            
+        }
+        // Double Jump
+        else
+        {
+             if (XCI.GetButtonDown(XboxButton.A) && CanJump)
+            {
+
+                moveDirection.y = Jump;
+                CanJump = false;
             }
         }
-        moveDirection.y = moveDirection.y + (Physics.gravity.y   * gravScale * Time.deltaTime);
+        if (IsSprinting && SprintToggle == 0)
+        {
+            Speed *= 2;
+            SprintToggle++;
+        }
+        if (IsSprinting == false)
+        {
+            Speed = DefaultSpeed;
+            SprintToggle = 0;
+        }
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravScale * Time.deltaTime);
         CharControl.Move(moveDirection * Time.deltaTime);
 
-         
+        if(XCI.GetAxis(XboxAxis.LeftTrigger) > 0.35)
+        {
+            IsSprinting = true;
+        }
+        else
+        {
+            IsSprinting = false;
 
+            Speed = DefaultSpeed;
+         }
     }
- 
+
+    
 }
 
